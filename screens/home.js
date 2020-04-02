52c34298a8aa775f2d17f23297b3315476176408
Temitope <temmieayodele@gmail.com>
2020-04-02 16:56:28 +0100

    changes

--- screens/home.js
+++ screens/home.js
@@ -7,11 +7,13 @@ import {
   TouchableOpacity,
   Modal,
   TouchableWithoutFeedback,
-  Keyboard
+  Keyboard,
+  Slider
 } from "react-native";
 import { globalStyles } from "../styles/global";
 import Card from "../shared/card";
 import { MaterialIcons } from "@expo/vector-icons";
+// import Slider from 'react-native';
 import AddTaskForm from "./addTaskForm";
 
 export default function Home({ navigation }) {
@@ -49,45 +51,107 @@ export default function Home({ navigation }) {
     }
   ]);
   const [completedTasks, setCompletedTasks] = useState([]);
+  const [progress, setProgress] = useState(0);
+  // const [val, setVal] = useState(props);
+
+  // useEffect(() => {
+  //     setVal(props);
+  // }, [props])
+
+  // useEffect(() => {
+  //   effect
+  //   return () => {
+  //     cleanup
+  //   }
+  // }, [input])
 
   const addTask = task => {
     task.key = Math.random().toString();
-    task.completed = false
+    task.completed = false;
     setTasks(currentTasks => {
       return [task, ...currentTasks];
     });
     setModalOpen(false);
   };
-
-  const markAsComplete = key => {
-    let copiedArray = [...tasks];
-    const idx = copiedArray.findIndex(task => task.key === key);
-    const item = copiedArray[idx];
+  const trackProgress = () => {
+    console.log(completedTasks.length, tasks.length);
+    const perc = (completedTasks.length / tasks.length) * 100;
+    setProgress(perc);
+    console.log(progress);
+  };
+  const markAsComplete = async key => {
+    const idx = tasks.findIndex(task => task.key === key);
+    console.log(idx);
+    const item = tasks[idx];
     if (!item.completed) {
       item.completed = true;
-      setCompletedTasks(currentTasks => {
-        return [...currentTasks, item];
+      setTasks(currentTasks => {
+        currentTasks.splice(idx, 1);
+        currentTasks.push(item);
+        return currentTasks;
       });
-      copiedArray.splice(idx, 1);
-      copiedArray.push(item);
-      setTasks(copiedArray);
-    } else {
+      setCompletedTasks(currentTasks => {
+        return [item, ...currentTasks];
+      });
+    } else if(item.completed){
       item.completed = false;
+    // console.log(item);
+     await setTasks(currentTasks => {
+        currentTasks.splice(idx, 1)
+        console.log(item)
+        return [item, ...currentTasks]
+      })
+      console.log(tasks)
+      // // console.log(item.completed);
       setCompletedTasks(currentTasks => {
         return currentTasks.filter(task => {
           task !== item;
         });
       });
-      copiedArray.splice(idx, 1);
-      copiedArray.unshift(item);
-      setTasks(copiedArray);
+      // setTasks(currentTasks => {
+      //   currentTasks.splice(idx, 1);
+      //   return [item, ...currentTasks];
+      // });
+      // console.log(item);
     }
+
+    // let copiedArray = [...tasks];
+    // const idx = copiedArray.findIndex(task => task.key === key);
+    // const item = copiedArray[idx];
+    // if (!item.completed) {
+    //   item.completed = true;
+    //   copiedArray.splice(idx, 1);
+    //   copiedArray.push(item);
+    //   setTasks(copiedArray);
+    //   // await setCompletedTasks(currentTasks => {
+    //   //   return [...currentTasks, item];
+    //   // });
+    //   // setCompletedTasks([...completedTasks, item])
+    //   completedTasks.push(item);
+    //   console.log(completedTasks);
+    // } else {
+    //   item.completed = false;
+    //   copiedArray.splice(idx, 1);
+    //   copiedArray.unshift(item);
+    //   setTasks(copiedArray);
+    //   await setCompletedTasks(currentTasks => {
+    //     return currentTasks.filter(task => {
+    //       task !== item;
+    //     });
+    //   });
+    // }
+    // console.log(completedTasks.length);
+
+    // trackProgress();
   };
 
   return (
-    <View style={{...globalStyles.container, ...styles.listContainer}}>
+    <View style={{ ...globalStyles.container, ...styles.listContainer }}>
       <Modal visible={modalOpen} animationType="slide">
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          {console.log(tasks.length, completedTasks.length)}
+
+          {console.log("the progress is :" + progress)}
           <View style={styles.modalContent}>
             <MaterialIcons
               name="close"
@@ -103,8 +167,19 @@ export default function Home({ navigation }) {
       <MaterialIcons
         name="add"
         size={24}
-        style={{...styles.modalToggle, ...styles.modalOpen}}
+        style={{ ...styles.modalToggle, ...styles.modalOpen }}
         onPress={() => setModalOpen(true)}
+      />
+
+      <Slider
+        style={{ width: 500, height: 100 }}
+        minimumValue={0}
+        maximumValue={100}
+        // step={50}
+        minimumTrackTintColor="green"
+        maximumTrackTintColor="#000000"
+        value={progress}
+        style={styles.slider}
       />
 
       <FlatList
@@ -130,7 +205,7 @@ export default function Home({ navigation }) {
 
 const styles = StyleSheet.create({
   listContainer: {
-    position: 'relative'
+    position: "relative"
   },
   modalContent: {
     flex: 1
@@ -148,12 +223,15 @@ const styles = StyleSheet.create({
     marginBottom: 0
   },
   modalOpen: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 60,
     zIndex: 1,
     right: 30,
     borderRadius: 50,
-    backgroundColor: 'rgba(255, 192, 203, 0.8)',
+    backgroundColor: "rgba(255, 192, 203, 0.8)",
     padding: 20
+  },
+  slider: {
+    backgroundColor: "red"
   }
 });
