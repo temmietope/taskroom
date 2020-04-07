e9f06407fd29e574762fec5e724723fcd9742180
Temitope <temmieayodele@gmail.com>
2020-04-07 23:21:02 +0100

    update

--- screens/addTaskForm.js
+++ screens/addTaskForm.js
@@ -1,8 +1,89 @@
+// import React, { useState } from "react";
+// import { TextInput, View, Text, Button } from "react-native";
+// import { globalStyles } from "../styles/global";
+// import { Formik } from "formik";
+// // import DateTimePicker from "@react-native-community/datetimepicker";
+// import * as yup from "yup";
+// import FlatButton from "../shared/button";
+// import moment from "moment";
+// import DateTimePicker from "react-native-modal-datetime-picker";
+
+// const taskSchema = yup.object({
+//   title: yup.string().required().min(4),
+//   body: yup.string().required().min(8),
+// });
+
+// export default function AddTaskForm({ addTask }) {
+//   const [isPickerVisible, setIsPickerVisible] = useState(false);
+//   const showDateTimePicker = () => {
+//     setIsPickerVisible(true);
+//   };
+
+//   const hideDateTimePicker = () => {
+//     setIsPickerVisible(false);
+//   };
+
+//   const handleDatePicked = (date) => {
+//     console.log("A date has been picked: ", date);
+//     hideDateTimePicker();
+//   };
+//   return (
+//     <View style={globalStyles.container}>
+//       <Formik
+//         initialValues={{ title: "", body: "" }}
+//         validationSchema={taskSchema}
+//         onSubmit={(values, actions) => {
+//           actions.resetForm();
+//           console.log(values);
+//           addTask(values);
+//         }}
+//       >
+//         {(props) => (
+//           <View>
+//             <TextInput
+//               style={globalStyles.input}
+//               placeholder="Task title"
+//               onChangeText={props.handleChange("title")}
+//               value={props.values.title}
+//               onBlur={props.handleBlur("title")}
+//             />
+//             <Text style={globalStyles.errorText}>
+//               {props.touched.title && props.errors.title}
+//             </Text>
+//             <TextInput
+//               multiline
+//               minHeight={100}
+//               style={globalStyles.input}
+//               placeholder="Task body"
+//               onChangeText={props.handleChange("body")}
+//               value={props.values.body}
+//               onBlur={props.handleBlur("body")}
+//             />
+//             <Text style={globalStyles.errorText}>
+//               {props.touched.body && props.errors.body}
+//             </Text>
+
+//             <Button title="Show DatePicker" onPress={showDateTimePicker} />
+//             <DateTimePicker
+//               isVisible={isPickerVisible}
+//               onConfirm={handleDatePicked}
+//               onCancel={hideDateTimePicker}
+//               mode="datetime"
+//             />
+
+//             <FlatButton text="submit" onPress={props.handleSubmit} />
+//           </View>
+//         )}
+//       </Formik>
+//     </View>
+//   );
+// }
+
 import React, { useState } from "react";
 import { TextInput, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 import { Formik } from "formik";
-import DateTimePicker from "@react-native-community/datetimepicker";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import * as yup from "yup";
 import FlatButton from "../shared/button";
 import moment from "moment";
@@ -11,112 +92,108 @@ const taskSchema = yup.object({
   title: yup.string().required().min(4),
   body: yup.string().required().min(8),
 });
-
 export default function AddTaskForm({ addTask }) {
-  const [show, setShow] = useState(false);
-  // const [date, setDate] = useState(new Date());
-  const [mode, setMode] = useState("date");
+  return (
+    <Formik
+      initialValues={{
+        title: "",
+        body: "",
+        myDate: moment().format("YYYY-MM-DD"),
+      }}
+      validationSchema={taskSchema}
+      onSubmit={(values, actions) => {
+        actions.resetForm();
+        console.log(values);
+        addTask(values);
+      }}
+    >
+      {({
+        handleSubmit,
+        values,
+        setFieldValue,
+        handleChange,
+        handleBlur,
+        touched,
+        errors,
+      }) => (
+        <MyForm
+          values={values}
+          setFieldValue={setFieldValue}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+          touched={touched}
+          errors={errors}
+        />
+      )}
+    </Formik>
+  );
+}
 
-  // const onChange = (event, selectedDate) => {
-  //   const currentDate = selectedDate || date;
-  //   console.log("selected date " + selectedDate);
-  //   console.log(event);
-  //   setShow(true);
-  //   setDate(currentDate);
-  //   console.log(date);
-  // };
-  const showMode = (currentMode) => {
-    setShow(true);
-    setMode(currentMode);
+export const MyForm = (props) => {
+  const {
+    handleSubmit,
+    values,
+    setFieldValue,
+    handleChange,
+    handleBlur,
+    touched,
+    errors,
+  } = props;
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
   };
 
-  const showDatepicker = () => {
-    showMode("date");
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
   };
 
-  const showTimepicker = () => {
-    console.log("hey");
-    showMode("time");
+  const handleConfirm = (date) => {
+    setFieldValue("myDate", moment(date).format("YYYY-MM-DD"));
+    console.log(date);
+    hideDatePicker();
   };
 
   return (
     <View style={globalStyles.container}>
-      <Formik
-        initialValues={{ title: "", body: "", selectedDate: new Date() }}
-        validationSchema={taskSchema}
-        onSubmit={(values, actions) => {
-          actions.resetForm();
-          console.log(values);
-          addTask(values);
-        }}
-      >
-        {(props) => (
-          <View>
-            <TextInput
-              style={globalStyles.input}
-              placeholder="Task title"
-              onChangeText={props.handleChange("title")}
-              value={props.values.title}
-              onBlur={props.handleBlur("title")}
-            />
-            <Text style={globalStyles.errorText}>
-              {props.touched.title && props.errors.title}
-            </Text>
+      <TextInput
+        style={globalStyles.input}
+        placeholder="Task title"
+        onChangeText={handleChange("title")}
+        value={values.title}
+        onBlur={handleBlur("title")}
+      />
+      <Text style={globalStyles.errorText}>
+        {touched.title && errors.title}
+      </Text>
 
-            <View>
-              <Button onPress={showDatepicker} title="Show date picker!" />
-            </View>
-            <View>
-              <Button onPress={showTimepicker} title="Show time picker!" />
-            </View>
+      <FlatButton onPress={showDatePicker} text=" date and time" />
+      <Text>{moment(values.myDate).format("YYYY-MM-DD")}</Text>
 
-            {/* {show && (
-              <DateTimePicker
-                testID="dateTimePicker"
-                timeZoneOffsetInMinutes={0}
-                value={date}
-                mode={mode}
-                is24Hour={true}
-                display="default"
-                onChange={onChange}
-                minimumDate= {new Date()}
-              />
-            )} */}
+      <TextInput
+        multiline
+        minHeight={100}
+        style={globalStyles.input}
+        placeholder="Task body"
+        onChangeText={handleChange("body")}
+        value={values.body}
+        onBlur={handleBlur("body")}
+      />
+      <Text style={globalStyles.errorText}>{touched.body && errors.body}</Text>
 
-            {show && (
-              <DateTimePicker
-                testID="dateTimePicker"
-                timeZoneOffsetInMinutes={0}
-                value={props.values.selectedDate}
-                mode={mode}
-                is24Hour={true}
-                display="default"
-                // onChange={onChange}
-                onChange={(event, selectedDate) => {
-                  // const currentDate = selectedDate || date;
-                  // setShow(true);
-                  props.handleChange("selectedDate");
-                }}
-                minimumDate={new Date()}
-              />
-            )}
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="datetime"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+        date={moment(values.myDate).toDate()}
+      />
 
-            <TextInput
-              multiline
-              minHeight={100}
-              style={globalStyles.input}
-              placeholder="Task body"
-              onChangeText={props.handleChange("body")}
-              value={props.values.body}
-              onBlur={props.handleBlur("body")}
-            />
-            <Text style={globalStyles.errorText}>
-              {props.touched.body && props.errors.body}
-            </Text>
-            <FlatButton text="submit" onPress={props.handleSubmit} />
-          </View>
-        )}
-      </Formik>
+      <FlatButton text="submit" onPress={handleSubmit} />
+
+      {/* <Button title="Submit" onPress={handleSubmit} /> */}
     </View>
   );
-}
+};
