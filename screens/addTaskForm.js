f4c13787f152e6334fcbead58ebf7ecca4011948
Temitope <temmieayodele@gmail.com>
2020-04-30 12:40:55 +0100

    update

--- screens/addTaskForm.js
+++ screens/addTaskForm.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { TextInput, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 import { Formik } from "formik";
-import DateTimePicker from "@react-native-community/datetimepicker";
 import * as yup from "yup";
 import FlatButton from "../shared/button";
 import moment from "moment";
@@ -13,36 +12,34 @@ const taskSchema = yup.object({
 });
 
 export default function AddTaskForm({ addTask }) {
-  const [show, setShow] = useState(false);
-  // const [date, setDate] = useState(new Date());
-  const [mode, setMode] = useState("date");
 
-  // const onChange = (event, selectedDate) => {
-  //   const currentDate = selectedDate || date;
-  //   console.log("selected date " + selectedDate);
-  //   console.log(event);
-  //   setShow(true);
-  //   setDate(currentDate);
-  //   console.log(date);
-  // };
-  const showMode = (currentMode) => {
-    setShow(true);
-    setMode(currentMode);
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+ 
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+ 
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+ 
+  const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    hideDatePicker();
   };
 
-  const showDatepicker = () => {
-    showMode("date");
-  };
 
-  const showTimepicker = () => {
-    console.log("hey");
-    showMode("time");
-  };
+
+
+
 
   return (
     <View style={globalStyles.container}>
       <Formik
-        initialValues={{ title: "", body: "", selectedDate: new Date() }}
+        initialValues={{
+          title: "",
+          body: "",
+        }}
         validationSchema={taskSchema}
         onSubmit={(values, actions) => {
           actions.resetForm();
@@ -63,44 +60,6 @@ export default function AddTaskForm({ addTask }) {
               {props.touched.title && props.errors.title}
             </Text>
 
-            <View>
-              <Button onPress={showDatepicker} title="Show date picker!" />
-            </View>
-            <View>
-              <Button onPress={showTimepicker} title="Show time picker!" />
-            </View>
-
-            {/* {show && (
-              <DateTimePicker
-                testID="dateTimePicker"
-                timeZoneOffsetInMinutes={0}
-                value={date}
-                mode={mode}
-                is24Hour={true}
-                display="default"
-                onChange={onChange}
-                minimumDate= {new Date()}
-              />
-            )} */}
-
-            {show && (
-              <DateTimePicker
-                testID="dateTimePicker"
-                timeZoneOffsetInMinutes={0}
-                value={props.values.selectedDate}
-                mode={mode}
-                is24Hour={true}
-                display="default"
-                // onChange={onChange}
-                onChange={(event, selectedDate) => {
-                  // const currentDate = selectedDate || date;
-                  // setShow(true);
-                  props.handleChange("selectedDate");
-                }}
-                minimumDate={new Date()}
-              />
-            )}
-
             <TextInput
               multiline
               minHeight={100}
@@ -113,6 +72,13 @@ export default function AddTaskForm({ addTask }) {
             <Text style={globalStyles.errorText}>
               {props.touched.body && props.errors.body}
             </Text>
+            <View>
+              <Text>today</Text>
+              <FlatButton text="Add date" />
+              <Text>time</Text>
+              <FlatButton text="Add time" />
+            </View>
+
             <FlatButton text="submit" onPress={props.handleSubmit} />
           </View>
         )}
